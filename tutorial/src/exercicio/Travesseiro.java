8fcc51d9743af0f14ad137e9384a3c46dbb2433e
Pehfos <pehfoz@gmail.com>
2024-12-07 02:53:54 -0300

    Update

--- tutorial/src/exercicio/Travesseiro.java
+++ tutorial/src/exercicio/Travesseiro.java
@@ -6,5 +6,80 @@ public class Travesseiro {
 	private String formato;
 	private String material;
 	private double peso;
+
+	public Travesseiro(String cor, String tamanho, String formato, String material, double peso){
+		this.cor = cor;
+		this.tamanho = tamanho;
+		this.formato = formato;
+		this.material = material;
+		this.peso = peso;
+	}
+
+	public String getCor() {
+		return this.cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getTamanho() {
+		return this.tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public String getFormato() {
+		return this.formato;
+	}
+
+	public void setFormato(String formato) {
+		this.formato = formato;
+	}
+
+	public String getMaterial() {
+		return this.material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public double getPeso() {
+		return this.peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public String toString(){
+		return "Travesseiro[cor="+cor+", tamanho="+tamanho+", formato="+formato+", material="+material+", peso="+peso+"]";
+	}
+
+	//Não consegui fazer o método condicional abaixo funcionar, dei uma olhada nas funções boleanas e nas condicionais mas não consegui fazer o que queria aqui.
+
+	//String sim = "Sim";
+
+	public void afofar(){
+		//if (sim == "Sim") {
+			System.out.println("O travesseiro foi afofado.");
+		//} else {
+			//System.out.println("O travesseiro não foi afoado.");
+		//}
+	}
+
+	public void cair(){
+		System.out.println("O travesseiro caiu.");
+	}
 	
+	public void jogar(){
+		System.out.println("O travesseiro foi jogado.");
+	}
+	
+	public void amassar(){
+		System.out.println("O travesseiro foi amassado.");
+	}
 }

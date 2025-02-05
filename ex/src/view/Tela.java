a1c9762e741512bf3b38817e22288ff990897530
PEDRO HENRIQUE PEREIRA FONSECA <pedro.fonseca@aluno.senac.rio>
2025-02-04 21:42:26 -0300

    Added stuff

--- ex/src/view/Tela.java
+++ ex/src/view/Tela.java
@@ -1,98 +1,89 @@
 package view;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import model.Tarefa;
 
-public class Tela {	
+public class Tela {
 	public void exibirMenu() {
 		System.out.println("----- MENU PRINCIPAL -----");
 		System.out.println("1 - Inclusão de tarefa");
 		System.out.println("2 - Consulta de tarefa");
 		System.out.println("3 - Alteração de tarefa");
 		System.out.println("4 - Exclusão de tarefa");
-		System.out.println("0 - F I M");
-		System.out.println("Digite a opçao desejada:");
+		System.out.println("9 - Carga de dados de teste");
+		System.out.println("0 - F  I  M");
+		System.out.println("Digite a opção desejada:");
 	}
 	
-
-	
-	public Tarefa exibirInclusao() {
+	public Tarefa exibirInclusao(int indice) {
 		Scanner teclado = new Scanner(System.in);
 		Tarefa tarefa = new Tarefa();
-		List<Tarefa> tarefas = new ArrayList<>();
 		
+		tarefa.setId(indice);
 		System.out.println("I N C L U S Ã O");
-		System.out.println("Descrição: ");
-		tarefa.setDescricao(teclado.nextLine()); 
-        System.out.println("Prazo: ");
-        tarefa.setPrazo(teclado.nextInt());
-        System.out.println("Finalizada (S/N)? ");
-        String status = teclado.next();
-        
-        if(status.equals("S") || status.equals("s")) {
-            tarefa.setFinalizada(true);
-        } else if(status.equals("N") || status.equals("n")) {
-            tarefa.setFinalizada(false);
-        } else {
-            System.out.println("Digite S ou N");
-        }
-	
-		tarefas.add(tarefa);
-		return tarefa;
+		System.out.println("Descrição : ");
+		tarefa.setDescricao(teclado.nextLine());
+		System.out.println("Prazo : ");
+		tarefa.setPrazo(teclado.nextInt());
+		System.out.println("Finalizada (S/N)? ");
+		String status = teclado.next();
 		
-	}
-	
-	public void exibirConsulta(Tarefa tarefa) { 
-		System.out.println("C O N S U L T A");
-		System.out.println(tarefa);
-		
-	}
-	
-	public Tarefa exibirAlteracao(Tarefa tarefa) {
-		Scanner teclado = new Scanner(System.in);
-		Scanner menu = new Scanner(System.in);
-				
-		System.out.println("O que deseja alterar?" + "\n" + "1 - Descrição" + "\n" + "2 - Prazo" + "\n" + "3 - Status");
-		
-		int opcao = menu.nextInt();
-		if(opcao == 1) {
-			System.out.println("Digite a nova Descrição (" + tarefa.getDescricao() + "): ");
-			tarefa.setDescricao(teclado.nextLine());
-			System.out.println("Descrição alterada.");
-		} else if(opcao == 2) {
-			System.out.println("Digite o novo Prazo (" + tarefa.getPrazo() + "): ");
-			tarefa.setPrazo(teclado.nextInt());
-			System.out.println("Prazo alterado.");
-		} else if(opcao == 3) {
-			System.out.println("Digite o novo Status (S/N) ((" + tarefa.isFinalizada() + ")): ");
-			String status = teclado.next();
-			
-			if(status.equals("S") || status.equals("s")) {
-				tarefa.setFinalizada(true);
-				System.out.println("Status alterado.");
-			} else if(status.equals("N") || status.equals("n")) {
-				tarefa.setFinalizada(false);
-				System.out.println("Status alterado.");
-			} else {
-				System.out.println("Digite S ou N");
-			} 
+		if(status.equals("S") || status.equals("s")) {
+			tarefa.setFinalizada(true);	
 		} else {
-			System.out.println("Opção inválida.");
+			tarefa.setFinalizada(false);
 		}
 		
 		return tarefa;
 	}
 	
-	public Tarefa exibirExclusao(Tarefa tarefa) {
-		tarefa.setDescricao("");
-		tarefa.setPrazo(0);
-		tarefa.setFinalizada(false);
-		System.out.println("Tarefa excluída.");
+	public void exibirTarefa(List<Tarefa> tarefas) {
+		System.out.println("C O N S U L T A");
+		
+		for(Tarefa item: tarefas) {
+			System.out.println(item);
+			System.out.println("--------------------------------------------");
+		}
+	}
+	
+	public Tarefa exibirAlteracao(Tarefa tarefa) {
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("A L T E R A Ç Ã O");
+		System.out.println("Descrição : ");
+		tarefa.setDescricao(teclado.nextLine());
+		System.out.println("Prazo : ");
+		tarefa.setPrazo(teclado.nextInt());
+		System.out.println("Finalizada (S/N)? ");
+		String status = teclado.next();
+		
+		if(status.equals("S") || status.equals("s")) {
+			tarefa.setFinalizada(true);	
+		} else {
+			tarefa.setFinalizada(false);
+		}
 		
 		return tarefa;
+	}
+	
+	public List<Tarefa> exibirExclusao(List<Tarefa> tarefas) {
+		Scanner teclado = new Scanner(System.in);
 		
+		exibirTarefa(tarefas);
+		
+		System.out.println("Digite o Id da tarefa que deseja excluir: ");
+		int x = teclado.nextInt();
+		Tarefa y = null;
+		
+		for(Tarefa tarefa: tarefas) {
+			if(tarefa.getId() == x) {
+				y = tarefa;
+			}
+		}
+		
+		tarefas.remove(y);
+
+		return tarefas;
 	}
 }
